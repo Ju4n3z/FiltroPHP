e687e983b7ac3acb77d415786e51610f2e3b658c
JuanEstebanGomezGarciaCAMPUS <juanesteban.gomezgarcia@gmail.com>
2023-06-28 17:57:34 -0500

    docs: :memo: Se agregó el README
    Se agregó el README del repositorio explicando como funciona y lo de la base de datos en terminal

--- scripts/db/connect.php
+++ scripts/db/connect.php
@@ -8,10 +8,10 @@
             try{
                 $this->con=new \PDO($_ENV["DSN"].":host=".$_ENV["HOST"].";dbname=".$_ENV["DBNAME"].";user=".$_ENV["USER"].";password=".$_ENV["PASSWORD"]);
                 $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                //$this->con->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
-                //$this->con->setAttribute(\PDO::ATTR_EMULATE_PREPARES, true);
-                //$this->con->setAttribute(\PDO::ATTR_PERSISTENT, true);
-                //$this->con->setAttribute(\PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
+                $this->con->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+                $this->con->setAttribute(\PDO::ATTR_EMULATE_PREPARES, true);
+                $this->con->setAttribute(\PDO::ATTR_PERSISTENT, true);
+                $this->con->setAttribute(\PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
             } catch(\PDOException $e){
                 echo "Connection failed". $e->getMessage();
             }
